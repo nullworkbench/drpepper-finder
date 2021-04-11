1b6068aea3cdbdc00e7845a23b2dc6fbd38c0ab5
nullworkbench <nullworkbench@gmail.com>
2021-04-12 00:53:59 +0900

    Initial

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.0'
+
+target 'drpepper-finder' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for drpepper-finder
+  pod 'Firebase/Firestore'
+  pod 'FirebaseFirestoreSwift'
+  pod 'Firebase/Auth'
+
+end
--- drpepper-finder/ViewController.swift
+++ drpepper-finder/ViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
+import FirebaseFirestore
 
 class ViewController: UIViewController {
+    
+    var authStateHandle: AuthStateDidChangeListenerHandle?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // FirebaseAuthの認証状態をリッスン
+        authStateHandle = Auth.auth().addStateDidChangeListener() { (auth, user) in
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        // FirebaseAuthの認証状態のリッスンを解除
+        Auth.auth().removeStateDidChangeListener(authStateHandle!)
     }
 
 
