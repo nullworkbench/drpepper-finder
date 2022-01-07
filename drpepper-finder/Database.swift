e6142e349e88e2091d1e84bd9951afb94bdd1340
nullworkbench <nullworkbench@gmail.com>
2022-01-07 22:28:03 +0900

    ピンのクラスを作成

---
+++ drpepper-finder/Database.swift
@@ -0,0 +1,16 @@
+//
+//  Database.swift
+//  drpepper-finder
+//
+//  Created by nullworkbench on 2022/01/07.
+//
+
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+class DB {
+    let db = Firestore.firestore()
+    
+    
+}
---
+++ drpepper-finder/Pin.swift
@@ -0,0 +1,33 @@
+//
+//  Pin.swift
+//  drpepper-finder
+//
+//  Created by nullworkbench on 2022/01/07.
+//
+
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+import CoreLocation
+
+class Pin {
+    // document ID
+    let docID: String!
+    // 座標
+    let coordinate: CLLocationCoordinate2D!
+    // 価格
+    let price: Int!
+    // 発見日時
+    let createdAt: Date!
+    // メモ
+    let note: String!
+    
+    // initialization
+    init(docID: String, geoPoint: GeoPoint, price: Int, createdAt: Date, note: String) {
+        self.docID = docID
+        self.coordinate = CLLocationCoordinate2DMake(geoPoint.latitude, geoPoint.longitude)
+        self.price = price
+        self.createdAt = createdAt
+        self.note = note
+    }
+    
+}
