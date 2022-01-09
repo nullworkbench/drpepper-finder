550014b848c03676dcc62067391cc514dccc4c3d
nullworkbench <nullworkbench@gmail.com>
2022-01-09 19:32:00 +0900

    同意する、同意しないボタンの処理を記述

--- drpepper-finder/controllers/FirstView/TermsOfServiceViewController.swift
+++ drpepper-finder/controllers/FirstView/TermsOfServiceViewController.swift
@@ -17,9 +17,13 @@ class TermsOfServiceViewController: UIViewController {
     
     // 同意しない
     @IBAction func disagreeBtnTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "サービスの利用には利用規約への同意が必要です。", message: "利用規約にご同意いただけない場合、サービスはご利用いただけません。", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
     // 同意する
     @IBAction func agreeBtnTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
 }
