6b0189b4a866527c7889b9d52e78afa3d7870f6b
nullworkbench <nullworkbench@gmail.com>
2022-01-09 19:11:09 +0900

    利用規約のビューを作成

--- drpepper-finder/controllers/FirstViewController.swift
+++ drpepper-finder/controllers/FirstViewController.swift
@@ -16,13 +16,32 @@ class FirstViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        // ログインしていない場合はLoginViewへ
-        if appDelegate.currentUser != nil {
-            print("Already logged in.")
-            performSegue(withIdentifier: "toMapView", sender: nil)
+        // 初回起動なら利用規約に同意してもらう
+        if true {
+            performSegue(withIdentifier: "toTermsOfServiceView", sender: nil)
         } else {
-            print("not logged in")
-            performSegue(withIdentifier: "toLoginView", sender: nil)
+            // ログインしていない場合はLoginViewへ
+            if appDelegate.currentUser != nil {
+                print("Already logged in.")
+                performSegue(withIdentifier: "toMapView", sender: nil)
+            } else {
+                print("not logged in")
+                performSegue(withIdentifier: "toLoginView", sender: nil)
+            }
+        }
+    }
+    
+    func isFirstLaunch() -> Bool {
+        // isFirstLaunchが存在するか
+        if UserDefaults.standard.object(forKey: "isFirstLaunch") != nil {
+            // 存在する場合
+            // 初回起動ではない
+            return false
+        } else {
+            // 存在しない場合
+            // 初回起動なので値をセット
+            UserDefaults.standard.set(false, forKey: "isFirstLaunch")
+            return true
         }
     }
 }
