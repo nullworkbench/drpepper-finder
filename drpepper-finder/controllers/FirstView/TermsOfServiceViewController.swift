239a6e7bd1d6a6c5c0b7e959ad09c35f575f64f8
nullworkbench <nullworkbench@gmail.com>
2022-01-09 19:14:34 +0900

    利用規約ビューのコントローラーを作成

---
+++ drpepper-finder/controllers/FirstView/TermsOfServiceViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TermsOfServiceViewController.swift
+//  drpepper-finder
+//
+//  Created by nullworkbench on 2022/01/09.
+//
+
+import UIKit
+
+class TermsOfServiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
