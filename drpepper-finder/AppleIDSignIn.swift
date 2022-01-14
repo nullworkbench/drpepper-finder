d418701c18b350b501d95f6e2f7cb09baca51030
nullworkbench <nullworkbench@gmail.com>
2022-01-14 19:23:16 +0900

    Appleでログイン機能を実装

---
+++ drpepper-finder/AppleIDSignIn.swift
@@ -0,0 +1,57 @@
+//
+//  AppleIDSignIn.swift
+//  drpepper-finder
+//
+//  Created by nullworkbench on 2022/01/13.
+//
+
+import AuthenticationServices
+import CryptoKit
+
+class AppleIDSignIn {
+    // Nonce生成関数
+    class func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError(
+                        "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+                    )
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    // ハッシュ関数
+    class func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
+    }
+}
--- drpepper-finder/controllers/FirstView/LoginViewController.swift
+++ drpepper-finder/controllers/FirstView/LoginViewController.swift
@@ -9,13 +9,22 @@ import UIKit
 import FirebaseCore
 import FirebaseAuth
 import GoogleSignIn
+import AuthenticationServices
+import CryptoKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var signInBtnStackView: UIStackView!
     @IBOutlet weak var SignInWithGoogleButton: UIButton!
+    
+    // AppleSignIn用
+    fileprivate var currentNonce: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Appleでログインボタンを設置
+        addAppleSignInBtn()
         
         GIDSignIn.sharedInstance()?.presentingViewController = self
         GIDSignIn.sharedInstance()?.delegate = self
@@ -23,10 +32,19 @@ class LoginViewController: UIViewController {
         changeAllSignInButtonStyle()
     }
     
+    // MapViewへ遷移
     func performToMapView() {
         self.dismiss(animated: true, completion: nil)
         self.presentingViewController?.performSegue(withIdentifier: "toMapView", sender: nil)
     }
+    
+    // ログインエラーアラート
+    func loginErrorAlert() {
+        let alert = UIAlertController(title: "ログインエラー", message: "ログイン時にエラーが発生しました。ネットワーク接続などを確かめてから再度お試しください。", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
     
     @IBAction func signInWithGoogle() {
         GIDSignIn.sharedInstance().signIn()
@@ -35,6 +53,7 @@ class LoginViewController: UIViewController {
     @IBAction func signInAsAnonymous() {
         Auth.auth().signInAnonymously() {(authResult, err) in
             if let err = err {
+                self.loginErrorAlert()
                 print("Error logging in as Anonymoous: \(err)")
             } else {
                 // success
@@ -71,12 +90,91 @@ extension LoginViewController: GIDSignInDelegate {
     }
 }
 
+// MARK: AppleIDSignIn
+extension LoginViewController: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    
+    // お作法
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
+    // Appleでログインボタンをタップしたとき
+    @objc func handleAuthorizationAppleIDButtonPress() {
+        // Nonceの生成
+        let nonce = AppleIDSignIn.randomNonceString()
+        currentNonce = nonce
+        
+        // Apple SignInを準備
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName]
+        // delegateなどの設定
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        // SignInをリクエスト
+        authorizationController.performRequests()
+    }
+    
+    
+    // 認証が完了したとき
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        // FireBaseでの認証を開始
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            // Nonceの確認
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            // 認証で取得したトークンの確認
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            // トークンを文字列に変換
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            
+            // Firebaseの認証情報を定義
+            let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+            // Firebaseにサインイン
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                if let err = error {
+                    // エラーアラートを表示
+                    self.loginErrorAlert()
+                    print(err.localizedDescription)
+                    return
+                }
+                // Firebaseへのサインイン完了
+                self.performToMapView()
+            }
+        }
+    }
+    
+    // 認証時にエラーが発生したとき
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // エラーアラートを表示
+        loginErrorAlert()
+    }
+    
+    
+}
+
+
 
 // MARK: SignInButton Style
 extension LoginViewController {
     
     func changeAllSignInButtonStyle() {
         self.applySignInButtonStyle(SignInWithGoogleButton)
+    }
+    
+    // Appleでログインボタンを追加
+    func addAppleSignInBtn() {
+        let authorizationButton = ASAuthorizationAppleIDButton(type: .signIn, style: .black)
+        authorizationButton.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
+        self.signInBtnStackView.addArrangedSubview(authorizationButton)
     }
     
     func applySignInButtonStyle(_ target: UIButton!) {
